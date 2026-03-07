b9af6c5212860eda884684c1b630ccd2a8411f90
Alexandre Pereira <alexandresp15@gmail.com>
2026-03-07 18:04:52 -0300

    Implementação do consumo da API Gutendex

--- src/main/java/br/com/alexandresp15/literalura/LiteraluraApplication.java
+++ src/main/java/br/com/alexandresp15/literalura/LiteraluraApplication.java
@@ -1,13 +1,20 @@
 package br.com.alexandresp15.literalura;
 
+import br.com.alexandresp15.literalura.service.ConsumoApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiteraluraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiteraluraApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        ConsumoApi consumo = new ConsumoApi();
+
+        String endereco = "https://gutendex.com/books/?search=dom+casmurro";
+
+        String json = consumo.obterDados(endereco);
+
+        System.out.println(json);
+    }
 }
